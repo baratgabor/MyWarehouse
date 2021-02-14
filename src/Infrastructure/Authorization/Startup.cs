7c3f6ac1e1446635818fa6beb0d31bafb7cbc722
Gábor Barát <barat.gabor@gmail.com>
2021-02-14 17:54:12 +0100

    Add reminder concerning authorization implementation

--- src/Infrastructure/Authorization/Startup.cs
+++ src/Infrastructure/Authorization/Startup.cs
@@ -4,6 +4,7 @@ using MyWarehouse.Infrastructure.Authorization.Constants;
 
 namespace MyWarehouse.Infrastructure.Authorization
 {
+    // TODO: Implement authorization system, actually preferably in Application layer; if a good, simple use case can be established in this sample project.
     internal static class Startup
     {
         public static void ConfigureServices(IServiceCollection services, IConfiguration _)
