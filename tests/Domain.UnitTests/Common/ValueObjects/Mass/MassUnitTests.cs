a98e8ace9658ed2f4209700ce573fc143c1a7830
Gábor Barát <barat.gabor@gmail.com>
2021-02-26 15:58:02 +0100

    Add some extra value object tests to domain

---
+++ tests/Domain.UnitTests/Common/ValueObjects/Mass/MassUnitTests.cs
@@ -0,0 +1,28 @@
+﻿using FluentAssertions;
+using MyWarehouse.Domain.Common.ValueObjects.Mass;
+using NUnit.Framework;
+using System;
+
+namespace MyWarehouse.Domain.UnitTests.Common.ValueObjects.MassTests
+{
+    public class MassUnitTests
+    {
+        [TestCase("kg")]
+        [TestCase("t")]
+        [TestCase("lb")]
+        [TestCase("g")]
+        public void FromSymbol_WithValidSymbol_ReturnsCorrectUnit(string symbol)
+        {
+            var result = MassUnit.FromSymbol(symbol);
+
+            result.Symbol.Should().Be(symbol);
+        }
+
+        [Test]
+        public void FromSymbol_WithInCorrectSymbol_ThrowsArgumentException()
+        {
+            FluentActions.Invoking(() => MassUnit.FromSymbol("abcabc"))
+                .Should().ThrowExactly<ArgumentException>();
+        }
+    }
+}
